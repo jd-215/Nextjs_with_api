bbc449445c20b295efc863937480612662b02093
jd-215 <58883072+jd-215@users.noreply.github.com>
2024-02-07 13:59:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig= {
 	env: {
-		BASE_URL: "http://localhost:3000",
-		USER_NAME: "jashDas",
-		PASSWORD: "SXJNJWP8xWYPOjUw",
-		JWT_SECRET_KEY: "ASCF3453GES34VD212",
-		MAIL_USERNAME: "783401bffc04a7",
-		MAIL_PASSWORD: "65cf8a95c308c7",
+		
 	},
 };
 
